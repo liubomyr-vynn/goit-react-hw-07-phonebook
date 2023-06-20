a3bc30694e7b1349340d0e18e238f8dfa6a2923e
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-06-20 16:47:11 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,19 @@ import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from '../redux/contactsSlice';
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div className="container">
+    <div className="app">
       <ContactForm />
       <Filter />
       <ContactList />
