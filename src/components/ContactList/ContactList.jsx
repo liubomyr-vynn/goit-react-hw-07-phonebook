f161c121655358ae0ad8d711f234dc477394b8d6
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-06-20 16:47:03 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,55 @@
+// import React from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { deleteContact } from '../../redux/contactsSlice';
+
+// const ContactList = () => {
+//   const contacts = useSelector(state => {
+//     const { contacts, filter } = state.contacts;
+//     if (filter === '') {
+//       return contacts;
+//     } else {
+//       return contacts.filter(contact =>
+//         contact.name.toLowerCase().includes(filter.toLowerCase())
+//       );
+//     }
+//   });
+
+//   const dispatch = useDispatch();
+
+//   const handleDelete = contactId => {
+//     dispatch(deleteContact(contactId));
+//   };
+
+//   return (
+//     <div className="section__list">
+//       <ul className="contact__list">
+//         {contacts.map(contact => (
+//           <li className="contact__item" key={contact.id}>
+//             <p className="contact__text">
+//               {contact.name}: {contact.number}
+//             </p>
+//             <button
+//               type="button"
+//               className="contact__button"
+//               onClick={() => handleDelete(contact.id)}
+//             >
+//               Delete
+//             </button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default ContactList;
+
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/contactsSlice';
 
 const ContactList = () => {
-  const contacts = useSelector(state => {
-    const { contacts, filter } = state.contacts;
-    if (filter === '') {
-      return contacts;
-    } else {
-      return contacts.filter(contact =>
-        contact.name.toLowerCase().includes(filter.toLowerCase())
-      );
-    }
-  });
-
+  const contacts = useSelector(state => state.contacts.items);
   const dispatch = useDispatch();
 
   const handleDelete = contactId => {
@@ -23,6 +59,7 @@ const ContactList = () => {
   return (
     <div className="section__list">
       <ul className="contact__list">
+        {' '}
         {contacts.map(contact => (
           <li className="contact__item" key={contact.id}>
             <p className="contact__text">
