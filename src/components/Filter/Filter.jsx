5c6958d770ce655877cfe0691bffb7e6b3a994e5
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-06-21 22:33:45 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,44 +1,14 @@
-// import React from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { setFilterValue } from '../../redux/contactsSlice';
-
-// const Filter = () => {
-//   const filterValue = useSelector(state => state.contacts.filter);
-//   const dispatch = useDispatch();
-
-//   const handleFilter = event => {
-//     const { value } = event.target;
-//     dispatch(setFilterValue(value));
-//   };
-
-//   return (
-//     <div className="section">
-//       <h2 className="section__title">Contacts</h2>
-//       <label htmlFor="filter">Find contacts by name</label>
-//       <input
-//         className="form__input"
-//         type="text"
-//         name="filter"
-//         id="filter"
-//         value={filterValue}
-//         onChange={handleFilter}
-//       />
-//     </div>
-//   );
-// };
-
-// export default Filter;
-
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { setFilterValue } from '../../redux/contactsSlice';
+import { setFilterValue } from '../../redux/operations';
 
 const Filter = () => {
-  const filter = useSelector(state => state.contacts.filter);
+  const filterValue = useSelector(state => state.contacts.filter);
   const dispatch = useDispatch();
 
   const handleChange = event => {
-    dispatch(setFilterValue(event.target.value));
+    const { value } = event.target;
+    dispatch(setFilterValue(value));
   };
 
   return (
@@ -48,7 +18,7 @@ const Filter = () => {
       <input
         className="form__input"
         type="text"
-        value={filter}
+        value={filterValue}
         onChange={handleChange}
         name="filter"
       />
